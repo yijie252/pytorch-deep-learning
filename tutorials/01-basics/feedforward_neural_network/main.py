bbd77008b25bbb589136a3a42a992e273e09e00f
yijie252 <1337187085@qq.com>
2024-08-27 10:58:11 +0800

    learning rate

--- tutorials/01-basics/feedforward_neural_network/main.py
+++ tutorials/01-basics/feedforward_neural_network/main.py
@@ -13,7 +13,7 @@ hidden_size = 500
 num_classes = 10
 num_epochs = 5
 batch_size = 100
-learning_rate = 0. 001
+learning_rate = 0.002
 
 # MNIST dataset 
 train_dataset = torchvision.datasets.MNIST(root='../../data', 
